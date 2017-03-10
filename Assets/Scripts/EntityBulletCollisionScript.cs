bbbcbe7a3c89399c36618b75a5ddae8fed25c182
blaze97531 <jrhansf@terpmail.umd.edu>
2017-03-09 19:52:34 -0500

    Added bullet collision detection on walls and a static spherical enemy that does nothing and can be destroyed.

---
+++ Assets/Scripts/EntityBulletCollisionScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityBulletCollisionScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.CompareTag ("Bullet")) {
+			Destroy (other.gameObject);
+			HealthTrackingScript thisObjectsHealth = gameObject.GetComponent<HealthTrackingScript> ();
+			thisObjectsHealth.inflictDamage (1.0f);
+		}
+	}
+}
---
+++ Assets/Scripts/HealthTrackingScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthTrackingScript : MonoBehaviour {
+
+	public float MAX_HEALTH;
+	private float current_health;
+
+	// Use this for initialization
+	void Start () {
+		current_health = MAX_HEALTH;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void inflictDamage (float amount) {
+		current_health -= amount;
+		if (current_health <= 0.0f) {
+			Destroy (gameObject);
+		}
+	}
+
+}
