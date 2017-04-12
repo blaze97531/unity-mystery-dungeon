b052d8c1d6992c43ef6176f9f536ea67b58c73ba
blaze97531 <jrhansf@terpmail.umd.edu>
2017-04-11 21:26:10 -0400

    Floor is now randomly generated as an irregular grid of walls and empty spaces.

--- Assets/Scripts/GenerateRoom.cs
+++ Assets/Scripts/GenerateRoom.cs
@@ -14,12 +14,127 @@ public class GenerateRoom : MonoBehaviour {
 		wallPrefab = Resources.Load<GameObject> ("Prefab/Wall");
 		emptyPrefab = Resources.Load<GameObject> ("Prefab/EmptyGameObject");
 
-		generateRandomRoom (transform.localPosition);
+//		generateRandomRoom (transform.localPosition);
+		Floor floor = new Floor(this, 10, 10);
+		floor.generateGameObjects ();
+	}
+		
+	private enum CellEdge {
+		EMPTY, WALL, DOOR
+	}
+
+	private static CellEdge randomCellEdge (float prob_empty, float prob_wall) {
+		float random = Random.value;
+
+		if (random <= prob_empty)
+			return CellEdge.EMPTY;
+		else if (random <= prob_empty + prob_wall)
+			return CellEdge.WALL;
+		else
+			return CellEdge.DOOR;
+	}
+
+	private class Cell {
+		public CellEdge pos_x_edge;
+		public CellEdge pos_z_edge;
+	}
+
+	private class Floor {
+		private GenerateRoom enclosingInstance;
+		private Cell[,] cells;
+		private int n_x, n_z;
+		private int[] delta_xs;
+		private int[] delta_zs;
+		private int[] cumulative_delta_xs; 
+		private int[] cumulative_delta_zs;
+
+		public Floor (GenerateRoom enclosingInstance, int n_x, int n_z) {
+			this.enclosingInstance = enclosingInstance;
+			this.n_x = n_x;
+			this.n_z = n_z;
+			// Precondition n_x and n_z are both even
+			if (n_x % 2 != 0 || n_z % 2 != 0) {
+				throw new UnityException ();
+			}
+
+			cells = new Cell[n_x,n_z];
+			for (int i = 0; i < n_x; i++) {
+				for (int j = 0; j < n_z; j++) {
+					cells[i, j] = new Cell();
+				}
+			}
+
+
+			// Step one: generate a grid with varying delta-x / delta-y
+			delta_xs = new int[n_x];
+			delta_zs = new int[n_z];
+			cumulative_delta_xs = new int[n_x + 1];
+			cumulative_delta_zs = new int[n_z + 1];
+
+			cumulative_delta_xs[0] = 0;
+			cumulative_delta_zs[0] = 0;
+
+			for (int i = 0; i < n_x; i++) {
+				delta_xs[i] = Random.Range(10, 20);
+				cumulative_delta_xs[i+1] = delta_xs[i] + cumulative_delta_xs[i];
+			}
+			for (int i = 0; i < n_z; i++) {
+				delta_zs[i] = Random.Range(10, 20);
+				cumulative_delta_zs[i+1] = delta_zs[i] + cumulative_delta_zs[i];
+			}
+
+
+			const float EMPTY_CHANCE = 0.3125f; // 5 in 16
+			const float WALL_CHANCE = 1.0f - EMPTY_CHANCE;
+			// Now generate 2 x 2 segments of the grid.
+			for (int i = 0; i < n_x; i += 2) {
+				for (int j = 0; j < n_z; j += 2) {
+					cells[i, j].pos_x_edge = randomCellEdge(EMPTY_CHANCE, WALL_CHANCE);
+					cells[i, j].pos_z_edge = randomCellEdge(EMPTY_CHANCE, WALL_CHANCE);
+					cells[i+1, j].pos_z_edge = randomCellEdge(EMPTY_CHANCE, WALL_CHANCE);
+					cells[i, j+1].pos_x_edge = randomCellEdge(EMPTY_CHANCE, WALL_CHANCE);
+
+					cells[i+1, j].pos_x_edge = CellEdge.WALL;
+					cells[i, j+1].pos_z_edge = CellEdge.WALL;
+					cells[i+1, j+1].pos_x_edge = CellEdge.WALL;
+					cells[i+1, j+1].pos_z_edge = CellEdge.WALL;
+				}
+			}
+
+			// Next, need to use Union find to add doors until the entire floor is traversable.
+		}
+
+		public void generateGameObjects () {
+			int total_x = cumulative_delta_xs [n_x];
+			int total_z = cumulative_delta_zs [n_z];
+
+			Vector3 floorCenter = new Vector3 (total_x / 2.0f, 0.0f, total_z / 2.0f);
+			GameObject ground = Instantiate<GameObject> (enclosingInstance.groundPrefab, floorCenter, Quaternion.identity, enclosingInstance.transform);
+			ground.transform.localScale = new Vector3 (total_x, enclosingInstance.groundPrefab.transform.localScale.y, total_z);
+
+			for (int i = 0; i < n_x; i++) {
+				for (int j = 0; j < n_z; j++) {
+
+					if (cells [i, j].pos_x_edge == CellEdge.WALL) {
+						Vector3 position = new Vector3 (cumulative_delta_xs [i] + delta_xs [i], enclosingInstance.wallPrefab.transform.position.y, cumulative_delta_zs [j] + delta_zs [j] / 2.0f);
+						GameObject wall = Instantiate<GameObject> (enclosingInstance.wallPrefab, position, Quaternion.identity, enclosingInstance.transform);
+						wall.transform.localScale = new Vector3 (enclosingInstance.wallPrefab.transform.localScale.x, enclosingInstance.wallPrefab.transform.localScale.y, delta_zs [j]);
+					} 
+
+					if (cells [i, j].pos_z_edge == CellEdge.WALL) {
+						Vector3 position = new Vector3 (cumulative_delta_xs [i] + delta_xs [i] / 2.0f, enclosingInstance.wallPrefab.transform.position.y, cumulative_delta_zs [j] + delta_zs [j]);
+						GameObject wall = Instantiate<GameObject> (enclosingInstance.wallPrefab, position, Quaternion.identity, enclosingInstance.transform);
+						wall.transform.localScale = new Vector3 (delta_xs[i], enclosingInstance.wallPrefab.transform.localScale.y, enclosingInstance.wallPrefab.transform.localScale.z);
+					} 
+
+				}
+			}
+		}
 	}
 
 	void generateRandomRoom (Vector3 roomCenter) {
-		int x_size = Random.Range (10, 30);
-		int z_size = Random.Range (10, 30);
+		int x_size = Random.Range (5, 10);
+		int z_size = Random.Range (5, 10);
 
 		GameObject ground = Instantiate<GameObject> (groundPrefab, roomCenter, Quaternion.identity, transform);
 		ground.transform.localScale = new Vector3 (x_size, groundPrefab.transform.localScale.y, z_size);
